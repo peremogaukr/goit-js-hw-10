f6c07e78780e08a46b12c24d95879f1c0b9a3b00
peremogaukr <volovikoi28@gmail.com>
2023-11-06 20:41:44 +0200

    Update cats-api.js

--- src/js/cats-api.js
+++ src/js/cats-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
-  'live_ 2z22lowlOCZyYMQAgSgynQnGkK5b5t 2ZGESuojagRki4t5VNeW93tSbAkel5 mnZd';
+  'live_ 2z22lowlOCZyYMQAgSgynQnGkK5b5t 2ZGESuojagRki4t5VNeW93tSbAkel5';
 
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
